b03320bad444c04d80dbfd4f092f92229cb2b0c2
witty-banner-1950 <asimsali95@gmail.com>
2019-05-17 22:42:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,17 +9,17 @@ def array_with_two_elements
 end
 
 def first_element(my_first_element)
-  taylor_swift = ["welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = taylor_swift[0]
 end
 
 def third_element(my_third_element)
-  taylor_swift = ["welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = taylor_swift[2]
 end
 
 def last_element(my_last_element)
-  taylor_swift = ["welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_last_element = taylor_swift[-1]
 end
 
