ec8b4defdc6771a8557f7b21fe3868b666232aeb
witty-banner-1950 <asimsali95@gmail.com>
2019-05-17 22:28:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,8 @@ def instantiate_new_array
   instantiate_new_array = []
   return instantiate_new_array
 end
+
+def array_with_two_elements
+  array_with_two_elements = [1,2]
+  return array_with_two_elements
+end
