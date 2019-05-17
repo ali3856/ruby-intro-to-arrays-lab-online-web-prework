77ee5835c47e8d941a1ea7ebe927fcb6dec1c921
witty-banner-1950 <asimsali95@gmail.com>
2019-05-17 22:35:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,16 @@ def array_with_two_elements
   array_with_two_elements = [1,2]
   return array_with_two_elements
 end
+
+def first_element(my_first_element)
+  taylor_swift = ["welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = taylor_swift.first
+end
+
+def third_element(my_third_element)
+end
+
+def last_element(my_last_element)
+end
+
+def first_element_with_array_methods
