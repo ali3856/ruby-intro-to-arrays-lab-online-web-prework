20af904a7eb3b61fc551f8bfa83c979723b6841e
witty-banner-1950 <asimsali95@gmail.com>
2019-05-17 22:36:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,4 +23,11 @@ def last_element(my_last_element)
   my_first_element = taylor_swift[-1]
 end
 
-def first_element_with_array_methods
+def first_element_with_array_methods(first_country)
+end
+
+def last_element_with_array_methods(last_country)
+end
+
+def length_of_array(length)
+end
