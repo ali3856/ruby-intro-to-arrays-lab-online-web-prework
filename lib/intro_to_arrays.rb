9430557cd166442947301e42ea81d8f0d359addd
witty-banner-1950 <asimsali95@gmail.com>
2019-05-17 22:44:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -24,6 +24,8 @@ def last_element(my_last_element)
 end
 
 def first_element_with_array_methods(first_country)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country = south_east_asia.first
 end
 
 def last_element_with_array_methods(last_country)
